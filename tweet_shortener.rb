1181211d690587380199a89633e1ca237d6e9328
Tommi Cox <tommicox@gmail.com>
2019-03-11 01:50:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,7 +31,7 @@ def bulk_tweet_shortener(tweet)
   end
 end
   
-  def selective_tweet_shorterner(tweet)
+  def selective_tweet_shortener(tweet)
     if tweet.length > 140
     word_substituter(tweet)
   else
