e6312f70878960946354ab5b0483d17fdf593f2b
Tommi Cox <tommicox@gmail.com>
2019-03-11 01:53:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,3 +38,11 @@ end
     tweet
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + '...'
+  else
+    tweet
+  end
+en
